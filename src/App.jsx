0a4d1048d9fb0b5abb4123c7742efc72b41fa724
AGS3 <moorthy.chandiran21@gmail.com>
2025-07-24 15:11:07 +0530

    chnages done in the home page required api implemnted

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,15 @@ import { Toaster } from "./components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./app/home/Home";
 import ThankYou from "./app/thankyou/ThankYou";
-
-
-
-
-
-
-
 const queryClient = new QueryClient()
-
 function App() {
   return (
     <>
        <QueryClientProvider client={queryClient}>
       <Toaster />
       <Routes >
-        {/* Login Page        */}
         <Route path="/" element={<Home />} />
-        {/* Dashboard  */}
         <Route path="/thankyou" element={<ThankYou />} />
-        {/* Registration  */}
-     
-        
       </Routes>
       </QueryClientProvider>
     </>
